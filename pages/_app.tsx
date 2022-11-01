3a92a322ec022025f795322d4aec307b914c0e3e
Ritik Ranjan <rritik772@outlook.com>
2022-11-01 19:14:27 +0530

    added bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+
+    // @ts-ignore
+    import("bootstrap/dist/js/bootstrap");
+
+  }, []);
+
   return <Component {...pageProps} />
 }
